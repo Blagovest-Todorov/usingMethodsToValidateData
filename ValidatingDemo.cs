dd2a701d70284d2253d64e1c7df79b5e7e68181c
Blagovest-Todorov <blagovest.stod@abv.bg>
2020-11-25 14:01:28 +0200

    adding some more explanations in comments //

--- ValidatingDemo.cs
+++ ValidatingDemo.cs
@@ -14,7 +14,7 @@ namespace _04.MethodsForValidatingData
             Console.Write("Minutes: ");
             int minutes = int.Parse(Console.ReadLine()); //we read the minutes from the console
 
-            bool isValidTime = ValidateHours(hours) && ValidateMinutes(minutes);
+            bool isValidTime = ValidateHours(hours) && ValidateMinutes(minutes); //We check if format hours and minutes is correct
             if (isValidTime)
             {
                 Console.WriteLine("The time is {0}:{1} now." , hours, minutes);
@@ -25,7 +25,7 @@ namespace _04.MethodsForValidatingData
             }            
         }
 
-        static bool ValidateMinutes(int minutes)
+        static bool ValidateMinutes(int minutes) //creating methods to check if minutes and hours are in correct format
         {
             bool result = (minutes >= 0) && (minutes <= 59);
             return result;
